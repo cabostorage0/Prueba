e6a1f1147f49b6f7611ee2f57275e092558ea5a4
cabostorage0 <cabostorage0@gmail.com>
2025-08-25 10:42:52 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,25 @@
+variable "project_name" {
+  type    = string
+  default = "devops-lab-free"
+}
 
-variable "project_name"  { type = string  default = "devops-lab-free" }
-variable "aws_region"    { type = string  default = "us-east-1" }
-variable "instance_type" { type = string  default = "t2.micro" }
-variable "image_tag"     { type = string  default = "latest" }
-variable "github_repo"   { type = string  default = "OWNER/REPO" }
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "image_tag" {
+  type    = string
+  default = "latest"
+}
+
+variable "github_repo" {
+  type    = string
+  default = "OWNER/REPO"
+}
+
